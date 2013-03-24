a07d21a459c8526b5198eb4e0ceab62029bd2260
Alex Kesling <alex.kesling@gmail.com>
2013-03-24 14:43:40 -0400

    WORKING !!!!

--- index.py
+++ index.py
@@ -10,7 +10,7 @@ headers = {
     "Authorization": "bearer t-4bab26c4-a2f7-49d6-85d2-cc179637385d",
 }
 
-h = httplib2.Http(".cache")
+h = httplib2.Http()
 class req:
     def GET(self, name):
         endpoint = "https://api.sandbox.inbloom.org/api/rest/v1.1/teachers/3abf2b1743a55952f4b558fbdd758d128b8850da_id/teacherSectionAssociations/sections"
--- static/assets/js/loadsections.js
+++ static/assets/js/loadsections.js
@@ -3,7 +3,10 @@ $(document).ready(function () {
         url: "/foo",
         contentType: "application/vnd.slc+json",
         success: function (data) {
-            alert(data);
+            sections = JSON.parse(data).map(function(d) {
+                return '<div class="row"><div class="span1"></div><div class="span9"><div class="well"><a href="class.html#user=userID#section=sectionID"><h3>'+d.uniqueSectionCode+'</h3></a>'+'<p>Class size: '+Math.floor(100*Math.random())+'</p><p>Class avg: '+Math.floor(100*Math.random())+'</p></div> </div></div>';
+            });
+            $("#sections").html(sections.join("\n"))
         },
         error: function(jqXHR, textStatus, errorThrown) {alert("failure");}
     });
